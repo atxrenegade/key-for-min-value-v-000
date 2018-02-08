f5ab803d820a2b7d0f650e21098d49d458fa8a6d
Harleigh Abel <harleighabel@gmail.com>
2018-02-08 04:18:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  min_val = nil 
+  min_val = nil
   hash.each do |key, value|
-    if value < min_val || min_val == nil
+    if min_val == nil || value < min_val 
       min_val = value
       key
     else
